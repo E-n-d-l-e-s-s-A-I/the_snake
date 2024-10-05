008652da59f50088e9b4b1d3ac06819bc54656c6
Omelchenko Maksim <omelchenko.ma@dvfu.ru>
2024-10-05 17:29:54 +1000

    delete singelton decorator

--- game/decorators/__init__.py
+++ game/decorators/__init__.py
@@ -4,17 +4,6 @@ from . import *
 from functools import wraps
 
 
-def singleton(cls):
-    """Decorator for singelton classes"""
-    instances = {}
-
-    def getinstance(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
-    return getinstance
-
-
 def collision_handler(other_cls):
     """Decorator for collisions handlers"""
     other_cls_name = other_cls.__name__.lower()
